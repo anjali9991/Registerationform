13189df70f08ac6bdefcc1b3f0b7a0290a103c16
anjali <anjali019991@gmail.com>
2018-06-28 11:28:30 +0530

    Registeration form

---
+++ java/com/example/anjali/registerationforms/Loginform_Activity.java
@@ -0,0 +1,14 @@
+package com.example.anjali.registerationforms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Loginform_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginform_);
+
+    }
+}
